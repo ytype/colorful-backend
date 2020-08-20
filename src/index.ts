b53ee0b40b8ce52b640bbeffde560b1d32534196
권신학 <ytype33@gmail.com>
2020-08-21 00:50:56 +0900

    ENH: remove ssl

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 import logger from './util/logger'
 import app from './app'
-import fs from 'fs'
-import https from 'https'
 
-const options = {
-	key: fs.readFileSync('./keys/private.pem'),
-	cert: fs.readFileSync('./keys/public.pem')
-
-}
-const port = Number(process.env.PORT || 5000)
-https.createServer(options, app).listen(port, () => {
+const port = 5000
+app.listen(port, () => {
     logger.info('Express server started on port: ' + port)
 })
