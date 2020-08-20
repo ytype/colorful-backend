b40f837a4bd6fdd5f245d97d13cdbb3498ce57c3
권신학 <ytype33@gmail.com>
2020-08-20 22:21:40 +0900

    BLD: build for prod

--- build.js
+++ build.js
@@ -6,7 +6,7 @@ try {
     // Remove current build
     fs.removeSync('./dist/');
     // Transpile the typescript files
-    const proc = childProcess.exec('tsc --build tsconfig.json');
+    const proc = childProcess.exec('tsc --build tsconfig.prod.json');
     proc.on('close', (code) => {
         if (code !== 0) {
             throw Error("Build failed")
